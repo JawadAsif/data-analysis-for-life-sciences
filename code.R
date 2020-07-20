39d310bc92911700d9297060f3b259628261eb17
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2020-07-20 20:21:39 +0600

    initial commit

---
+++ code.R
@@ -0,0 +1,8 @@
+install.packages("downloader")
+library(downloader)
+url <- "https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/femaleMiceWeights.csv"
+filename <- "femaleMiceWeights.csv" 
+download(url, destfile=filename)
+dat <- read.csv("femaleMiceWeights.csv")
+dat
+dat[12,2]
