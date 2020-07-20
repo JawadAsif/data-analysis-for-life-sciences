9a27ceafc6a740fc739c5ae3164f74d03a9a3e0e
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2020-07-20 22:55:55 +0600

    done homework2

--- code.R
+++ code.R
@@ -6,3 +6,12 @@ download(url, destfile=filename)
 dat <- read.csv("femaleMiceWeights.csv")
 dat
 dat[12,2]
+dat$Bodyweight
+dat[["Bodyweight"]]
+dat$Bodyweight[11]
+View(dat)
+mean(dat$Bodyweight[13:24])
+?sample
+set.seed(1)
+sample(13:24,1)
+dat$Bodyweight[21]
