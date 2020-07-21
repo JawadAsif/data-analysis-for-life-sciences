72de9594cd49b6af9215f0b5365160123edc650c
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2020-07-21 23:09:14 +0600

    hw week 1

--- code.R
+++ code.R
@@ -15,3 +15,36 @@ mean(dat$Bodyweight[13:24])
 set.seed(1)
 sample(13:24,1)
 dat$Bodyweight[21]
+
+install.packages("dplyr")
+library(dplyr)
+controls <- filter(dat,Diet=="chow")
+controls <- select(dat,Bodyweight)
+class(controls)
+controls
+unlist(controls)
+str(controls)
+controls <- filter(dat,Diet=="chow") %>% select(Bodyweight) %>% unlist
+controls
+
+url="https://raw.githubusercontent.com/genomicsclass/dagdata/master/inst/extdata/msleep_ggplot2.csv"
+filename <- basename(url)
+download(url,filename)
+dat2 <- read.csv("msleep_ggplot2.csv")
+View(dat2)
+class(dat2)
+primates <- filter(dat2,order=="Primates")
+View(primates)
+nrow(primates)
+class(primates)
+primates1 <- filter(dat2,order=="Primates") %>% 
+  select(sleep_total)
+View(primates1)
+class(primates1)
+primates2 <- filter(dat2,order=="Primates") %>% 
+  select(sleep_total) %>% unlist %>% mean
+p <- summarize(primates)
+View(p)
+?summarise
+summarise(primates)
+dat2 %>%  filter(order=="Primates") %>% summarise(sleep_total) %>% unlist %>% mean
