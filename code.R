405499335c5d390738cf63e7deaec31e526f89dc
Md. Jawad Noor Asif <jawad.asif.bd@gmail.com>
2020-07-23 09:43:29 +0600

    box-plot homework

--- code.R
+++ code.R
@@ -48,3 +48,66 @@ View(p)
 ?summarise
 summarise(primates)
 dat2 %>%  filter(order=="Primates") %>% summarise(sleep_total) %>% unlist %>% mean
+
+
+load("skew.RData")
+dim(dat)
+View(dat)
+class(dat)
+par(mfrow = c(3,3))
+for (i in 1:9) {
+  qqnorm(dat[,i])
+}
+
+?hist
+seq(floor(dat[,4]),ceiling(dat[,4]),0.1)
+floor(dat[,4])
+hist(dat[,4],breaks = 
+       seq(floor(min(dat[,4])),
+           ceiling(max(dat[,4]))))
+
+?par
+par(mfrow=c(1,1))
+
+hist(dat[,9],breaks = 
+       seq(floor(min(dat[,9])),
+           ceiling(max(dat[,9]))))
+
+# box-plot
+head(InsectSprays)
+View(InsectSprays)
+?boxplot
+boxplot(InsectSprays)
+?split
+boxplot(split(InsectSprays$count,10))
+boxplot(InsectSprays$count,InsectSprays$spray)
+x <- split(InsectSprays,InsectSprays$spray)
+x
+par(mfrow=c(3,2))
+boxplot(x)
+?boxplot
+boxplot(split(InsectSprays,InsectSprays$spray))
+y <- split(InsectSprays$count,InsectSprays$spray)
+boxplot(y)
+par(mfrow=c(1,1))
+
+library(dplyr)
+install.packages("UsingR")
+data(nym.2002, package="UsingR")
+View(nym.2002)
+hist(nym.2002$time)
+?hist
+x <- split(nym.2002$time,nym.2002$gender)
+x
+hist(x$Male)
+hist(x$Female)
+boxplot(x)
+qqnorm(x$Male)
+qqline(x$Male)
+qqnorm(x$Female)
+qqline(x$Female)
+mean(x$Male)
+mean(x$Female)
+sd(x$Male)
+sd(x$Female)
+
